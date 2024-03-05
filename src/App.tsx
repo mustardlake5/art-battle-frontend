fa380f6b6551d2f7384fee6e09c29e66eaffb629
mustardlake5 <mustardlake5@gmail.com>
2024-03-05 21:03:48 +0900

    socketのconnectを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect } from "react";
 import Footer from "./page/Footer";
 import Header from "./page/Header";
 import Main from "./page/Main";
-import { Phase } from "./slices/statusSlice";
+import { socket } from "./lib/socket";
+import { useAppSelector } from "./app/hooks";
 
 function App() {
-  const phase = Phase.PURCHASE;
+  const phase = useAppSelector((state) => state.status.phase);
+
+  useEffect(() => {
+    function onConnect() {
+      console.log("connected: " + socket.id);
+    }
+
+    function onDisconnect() {
+      console.log("disconnected");
+    }
+
+    socket.connect();
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("socket.off: connect", onConnect);
+      socket.off("socket.off: disconnect", onDisconnect);
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="flex flex-col h-screen">
       <Header phase={phase} />
--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -12,7 +12,7 @@ type MainProps = {
 const Main = ({ phase }: MainProps) => {
   const { INITIAL, SEARCH_ENEMY, PURCHASE, BATTLE, RESULT } = Phase;
   return (
-    <main className="flex-1">
+    <main className="flex-1 container mx-auto">
       {phase === INITIAL ? (
         <Initial />
       ) : phase === SEARCH_ENEMY ? (
