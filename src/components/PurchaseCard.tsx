d017ce733f2de330f6fcebafa24c6fdd4912f80a
mustardlake5 <mustardlake5@gmail.com>
2024-03-08 18:46:25 +0900

    購入フェーズの相手の状態を表すコンポーネント(EnemyStatus.tsx)を使い回しできる形に修正

--- src/components/PurchaseCard.tsx
+++ src/components/PurchaseCard.tsx
@@ -1,11 +1,12 @@
+import { Phase } from "../slices/statusSlice";
 import OwnStatus from "./OwnStatus";
-import EnemyStatus from "./EnemyStatus";
+import EnemyStatus from "./utils/EnemyStatus";
 
 const PurchaseCard = () => {
   return (
     <div className="flex mt-10 min-h-[75vh] border border-stone-300 rounded-lg shadow-lg">
       <OwnStatus />
-      <EnemyStatus />
+      <EnemyStatus phase={Phase.PURCHASE} />
     </div>
   );
 };
