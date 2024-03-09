226ce1ddd5f17e722fd6d5153930da9a910fe987
mustardlake5 <mustardlake5@gmail.com>
2024-03-10 00:31:55 +0900

    soundのmute機能を実装

--- src/components/HeaderBase.tsx
+++ src/components/HeaderBase.tsx
@@ -1,3 +1,7 @@
+import { Volume2, VolumeX } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { toggleMute } from "../slices/soundSlice";
+
 type HeaderBaseProps = {
   bgColor: string;
   textColor: string;
@@ -5,9 +9,19 @@ type HeaderBaseProps = {
 };
 
 const HeaderBase = ({ bgColor, textColor, title }: HeaderBaseProps) => {
+  const mute = useAppSelector((state) => state.sound.mute);
+  const dispatch = useAppDispatch();
+
+  function handleSound() {
+    dispatch(toggleMute());
+  }
+
   return (
-    <header className={`${bgColor} ${textColor} p-5`}>
+    <header className={`relative ${bgColor} ${textColor} p-5`}>
       <h1 className="text-center text-xl font-bold">{title}</h1>
+      <button onClick={handleSound} className="absolute top-5 right-5">
+        {mute ? <VolumeX /> : <Volume2 />}
+      </button>
     </header>
   );
 };
